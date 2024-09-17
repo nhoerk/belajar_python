37b95a083acad86a197491393e427fae57803e2b
nhoerk <nhoerk@gmail.com>
2024-09-17 13:17:19 +0700

    update project

--- project_1/main.py
+++ project_1/main.py
@@ -59,5 +59,65 @@ def main():
             Fermi = Satu digit benar dan pada posisi yang tepat
             Begel = tidak ada digit yang benar
           
+            misalnya, jika nomor rahasia adalah 248 dan tebakan anda 843, petunjuknya adalah Fermi, Pico.
+            """).format(NUM_DIGITS)
+    
+    while True: #Main game loop
+        secretNum = getSecretNum()
+        print("Saya sudah menemukan sebuah nomor.")
+        print("anda harus menebak {} untuk mendapatkannya".format(MAX_GUESSES))
 
-            """)
+        numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
+            guess = ''
+            # terus lakukan pengulangan hingga tebakan valid yang diinput:
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print('Tebakan #{}: '.format(numGuesses))
+                guess = input('>')
+
+            clues = getCluess(guess, secretNum)
+            print(clues)
+            numGuesses += 1
+
+            if guess == secretNum:
+                break
+            if numGuesses > MAX_GUESSES:
+                print("Tebakan anda habis.")
+                print("Jawabannya adalah {}.".format(secretNum))
+        
+        print("Apakah kamu ingin bermain lagi? (ya atau tidak)")
+        if not input('> ').lower().startswith('y'):
+            break
+    print("Terima kasih sudah bermain!.")
+
+
+def getSecretNum():
+    numbers = list("0123456789")
+    random.shuffle(numbers)
+
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+def getCluess(guess, secretNum):
+    if guess == secretNum:
+        return 'Kamu mendapatkanya'
+
+    clues = []
+
+    for i in range(len(guess)):
+        if(guess[i] == secretNum[i]):
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            clues.append('Pico')
+    if len(clues) == 0:
+        return 'Bagels'
+    else:
+        clues.sort()
+        return ''.join(clues)
+
+
+if __name__ == '__main__':
+    main()      
+        
