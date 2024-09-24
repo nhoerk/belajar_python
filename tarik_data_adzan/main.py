de87ec01414fe039a2d605d8f5a694ecac459ca8
nhoerk <nhoerk@gmail.com>
2024-09-24 16:03:45 +0700

    update

--- tarik_data_adzan/main.py
+++ tarik_data_adzan/main.py
@@ -2,62 +2,6 @@ import requests
 from http.cookies import SimpleCookie
 import datetime
 
-import time
-
-bot_token = ''
-chat_id = ''
-
-def kirim_telegram(bot_token, chat_id):
-    url = f'https://api.telegram.org/bot{bot_token}/getUpdates'
-    response = requests.get(url)
-    # Print status code and response
-    # print('Status Code:', response.status_code)
-    # print('Response:', response.json())
-
-    response = requests.get(url)
-    data = response.json()
-
-    # Cek apakah respons OK dan ada hasil
-    if data['ok'] and data['result']:
-        # Ambil data id dari respons
-        chat_id = data['result'][0]['message']['chat']['id']
-
-    
-    # URL API Telegram
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-
-
-    # Pesan yang akan dikirim
-    message = f"""
-    *JADWAL SHALAT*
-    *Wilayah DKI Jakarta & Sekitarnya*
-    *{times['tanggal']} M*
-    
-    SUBUH     : {times['subuh']}
-    TERBIT      : {times['terbit']}
-    DHUHA    : {times['dhuha']}
-    DZUHUR   : {times['dzuhur']}
-    ASAR        : {times['ashar']}
-    MAGHRIB : {times['maghrib']}
-    ISYA          : {times['isya']}
-    """
-
-    # Parameter yang dikirim ke API
-    params = {
-        'chat_id': chat_id,
-        'text': message
-    }
-
-    # Mengirim permintaan POST
-    response = requests.post(url, params=params)
-
-    # Mengecek status respons
-    if response.status_code == 200:
-        print('Message sent successfully!')
-    else:
-        print('Failed to send message:', response.text)
-
-
 # Function to get PHPSESSID from the header of the response
 def get_session() -> str:
     url = 'https://bimasislam.kemenag.go.id/jadwalshalat'
@@ -173,5 +117,3 @@ for times in pray_times:
                 MAGHRIB : {times['maghrib']}
                 ISYA          : {times['isya']}
                 """
-        
-        kirim_wa(message)
