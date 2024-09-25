a2c5180b09869356eeb92ba26961125cbc89da1c
nhoerk <nhoerk@gmail.com>
2024-09-25 14:16:30 +0700

    insert code baru

--- daily_dev/AI_Scrape/main.py
+++ daily_dev/AI_Scrape/main.py
@@ -1,5 +1,11 @@
-# import streamlit as st
+import streamlit as st
+from scrape import scrape_website
 
-# st.title('AI web Scraper')
+st.title('AI web Scraper')
+url = st.text_input("Enter a Website URL: ")
 
-print('test')
+if st.button("Scrape Site"):
+    st.write("Scraping the website")
+    result  = scrape_website(url)
+    
+    print(result) 
